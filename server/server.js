740d26c27e431c1802b93bc5216f1f706587ee2b
dosepehr <dosepehr@gmail.com>
2023-04-01 04:09:39 +0430

    connecting to db

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 import express from 'express';
 import morgan from 'morgan';
-
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 const server = express();
+dotenv.config({ path: './config.env' });
 
-server.use(morgan('dev'))
+// * middlewares
+server.use(morgan('dev'));
+server.use(cors());
+server.use(bodyParser.json());
 
-server.get('/', (req, res) => {
-    console.log('first')
-})
+// * connection
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('db connected');
+});
+mongoose.Promise = global.Promise;
 
 
-server.listen(3000, () => {
-    console.log('server runnging on port 3000');
+
+const port = process.env.PORT || 5080;
+server.listen(port, () => {
+    console.log(`server runnging on port ${port}`);
 });
