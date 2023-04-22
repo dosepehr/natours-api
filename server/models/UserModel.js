15556031984e2a6111b8fa1bff284c335439b7b7
dosepehr <dosepehr@gmail.com>
2023-04-23 01:54:00 +0430

    add hashing user password

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 const UserSchema = mongoose.Schema({
     name: {
         type: String,
@@ -37,6 +38,13 @@ const UserSchema = mongoose.Schema({
             message: 'passwords are not the same',
         },
     },
+});
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.confirmPassword = undefined;
+    next();
 });
 
 let User = mongoose.model('Users', UserSchema);
