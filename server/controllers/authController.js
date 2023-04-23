a2efe4d9f03baa7288bd9c6731370a63602c25d3
dosepehr <dosepehr@gmail.com>
2023-04-23 14:16:55 +0430

    edit signup logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,14 @@
 import User from '../models/UserModel.js';
-import {catchAsync} from '../utils/catchAsync.js';
+import { catchAsync } from '../utils/catchAsync.js';
 
-
-export const signup = catchAsync(async (req, res,next) => {
-    const newUser = await User.create(req.body);
+export const signup = catchAsync(async (req, res, next) => {
+    const { name, email, password, passwordConfirm } = req.body;
+    const newUser = await User.create({
+        name,
+        email,
+        password,
+        passwordConfirm,
+    });
     res.status(201).json({
         status: 'success',
         user: newUser,
