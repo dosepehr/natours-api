8a186b8f00a915f7defe87f8eb5d5083828561b8
dosepehr <dosepehr@gmail.com>
2023-04-23 14:38:05 +0430

    add jwt to signup user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,16 +1,23 @@
 import User from '../models/UserModel.js';
 import { catchAsync } from '../utils/catchAsync.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config({ path: './config.env' });
 
 export const signup = catchAsync(async (req, res, next) => {
-    const { name, email, password, passwordConfirm } = req.body;
+    const { name, email, password, confirmPassword } = req.body;
     const newUser = await User.create({
         name,
         email,
         password,
-        passwordConfirm,
+        confirmPassword,
+    });
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES,
     });
     res.status(201).json({
         status: 'success',
+        token,
         user: newUser,
     });
 });
