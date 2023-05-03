f0fcc522af9832eb7b07a17e2f64618951415202
dosepehr <dosepehr@gmail.com>
2023-05-03 21:55:29 +0430

    add getOne to handlerFactory

--- server/controllers/handlerFactory.js
+++ server/controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 import { catchAsync } from '../utils/catchAsync.js';
 import ErrorHandler from '../utils/errorHandler.js';
+import APIFeatures from '../utils/apiFeatures.js';
 
 export const deleteOne = (Model) =>
     catchAsync(async (req, res, next) => {
@@ -25,7 +26,9 @@ export const updateOne = (Model) =>
         });
 
         if (!doc) {
-            return next(new AppError('No document found with that ID', 404));
+            return next(
+                new ErrorHandler('No document found with that ID', 404)
+            );
         }
 
         res.status(200).json({
@@ -41,5 +44,47 @@ export const createOne = (Model) =>
         res.status(201).json({
             status: 'success',
             data: doc,
+        });
+    });
+
+export const getOne = (Model, popOptions) =>
+    catchAsync(async (req, res, next) => {
+        let query = Model.findById(req.params.id);
+        if (popOptions) query = query.populate(popOptions);
+        const doc = await query;
+
+        if (!doc) {
+            return next(
+                new ErrorHandler('No document found with that ID', 404)
+            );
+        }
+
+        res.status(200).json({
+            status: 'success',
+            data: doc,
+        });
+    });
+
+export const getAll = (Model) =>
+    catchAsync(async (req, res, next) => {
+        // To allow for nested GET reviews on tour (hack)
+        let filter = {};
+        if (req.params.tourId) filter = { tour: req.params.tourId };
+
+        const features = new APIFeatures(Model.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        // const doc = await features.query.explain();
+        const doc = await features.query;
+
+        // SEND RESPONSE
+        res.status(200).json({
+            status: 'success',
+            results: doc.length,
+            data: {
+                data: doc,
+            },
         });
     });
