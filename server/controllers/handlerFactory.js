1b239a5e61ffdd7cd19d36a2642fcf165de4946a
dosepehr <dosepehr@gmail.com>
2023-05-03 20:20:53 +0430

    add updateOne to handlerFactory

--- server/controllers/handlerFactory.js
+++ server/controllers/handlerFactory.js
@@ -14,5 +14,22 @@ export const deleteOne = (Model) =>
         res.status(204).json({
             status: 'success',
             data: null,
-        }); 
+        });
+    });
+
+export const updateOne = (Model) =>
+    catchAsync(async (req, res, next) => {
+        const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        if (!doc) {
+            return next(new AppError('No document found with that ID', 404));
+        }
+
+        res.status(200).json({
+            status: 'success',
+            data: doc,
+        });
     });
