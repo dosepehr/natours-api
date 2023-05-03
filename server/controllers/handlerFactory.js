b2dfdf648a0f992205da4979929e0808753d4419
dosepehr <dosepehr@gmail.com>
2023-05-03 20:36:10 +0430

    add createOne to handlerFactory

--- server/controllers/handlerFactory.js
+++ server/controllers/handlerFactory.js
@@ -33,3 +33,13 @@ export const updateOne = (Model) =>
             data: doc,
         });
     });
+
+export const createOne = (Model) =>
+    catchAsync(async (req, res, next) => {
+        const doc = await Model.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: doc,
+        });
+    });
