470fb0d70cfb3ae5d8e06d245f40946978446889
dosepehr <dosepehr@gmail.com>
2023-05-06 01:40:59 +0430

    make overview page

--- server/controllers/viewController.js
+++ server/controllers/viewController.js
@@ -1,11 +1,16 @@
-export const overview = (req, res) => {
+import Tour from '../models/TourModel.js';
+import { catchAsync } from '../utils/catchAsync.js';
+export const overview = catchAsync(async (req, res, next) => {
+    // 1) get data from server
+    const tours = await Tour.find();
+
     res.status(200).render('overview', {
         title: 'All tours',
+        tours,
     });
-};
+});
 export const tour = (req, res) => {
     res.status(200).render('tour', {
         title: 'All tours',
     });
 };
-
