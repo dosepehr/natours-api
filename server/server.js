a313a9b0947d24373a0cc0ed3c2e8d45872677c2
dosepehr <dosepehr@gmail.com>
2023-05-05 19:40:28 +0430

    set up pug

--- server/server.js
+++ server/server.js
@@ -9,19 +9,18 @@ import hpp from 'hpp';
 import mongoSanitize from 'express-mongo-sanitize';
 import xss from 'xss-clean';
 import rateLimit from 'express-rate-limit';
-
 import tourRoute from './routes/TourRoute.js';
 import userRoute from './routes/userRoute.js';
 import ErrorHandler from './utils/errorHandler.js';
 import reviewRoute from './routes/ReviewRoute.js';
 import { errorController } from './controllers/errorController.js';
-
 dotenv.config({ path: './config.env' });
 
 const server = express();
-// * middlewares
-// ! security
-// ! limiting requests
+server.set('view engine', 'pug');
+server.set('views', './views');
+// security
+// limiting requests
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
@@ -35,14 +34,14 @@ if (process.env.NODE_ENV == 'development') {
 }
 
 server.use(cors());
-// ! body-parser for getting date from req.body
+// body-parser for getting date from req.body
 server.use(bodyParser.json({ limit: '10kb' }));
 
-// * data sanitization against NoSQL query injection
+// data sanitization against NoSQL query injection
 server.use(mongoSanitize());
-// * data sanitization against XSS
+// data sanitization against XSS
 server.use(xss());
-// * Prevent parameter pollution
+// Prevent parameter pollution
 server.use(
     hpp({
         whitelist: [
@@ -56,24 +55,27 @@ server.use(
     })
 );
 
-// * connection
+// connection
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('db connected');
 });
 mongoose.Promise = global.Promise;
-// * routes
+// routes
 server.use('/api/v1/tours', tourRoute);
 server.use('/api/v1/users', userRoute);
 server.use('/api/v1/reviews', reviewRoute);
 
-// * a route for undefined routes
+server.get('/', (req, res) => {
+    res.status(200).render('base');
+});
+// a route for undefined routes
 server.all('*', (req, res, next) => {
     next(new ErrorHandler(`can't fint ${req.originalUrl} on this server`, 404));
 });
 
 server.use(errorController);
 
-// * run server on port
+// run server on port
 const port = process.env.PORT || 5080;
 server.listen(port, () => {
     console.log(`server running on port ${port}`);
