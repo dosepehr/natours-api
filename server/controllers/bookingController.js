f63f7a0b9261e0939e6c3db298b8b955fd8b3dc3
dosepehr <dosepehr@gmail.com>
2023-05-13 16:44:44 +0330

    add my-tours route

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,9 +1,8 @@
 import Tour from '../models/TourModel.js';
 import { catchAsync } from '../utils/catchAsync.js';
-import { createOne } from './handlerFactory.js';
+import { createOne, getAll } from './handlerFactory.js';
 import ErrorHandler from '../utils/errorHandler.js';
 import Booking from '../models/BookingModel.js';
-
 
 export const setBookingData = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.tourId);
@@ -12,6 +11,19 @@ export const setBookingData = catchAsync(async (req, res, next) => {
     req.body.price = tour.price;
 
     next();
+});
+
+export const myTours = catchAsync(async (req, res, next) => {
+    const bookings = await Booking.find({
+        user: req.user.id,
+    });
+    const tourIDs = bookings.map((el) => el.tour);
+    const tours = await Tour.find({ _id: { $in: tourIDs } });
+    res.status(200).json({
+        status: 'success',
+        length: tours.length,
+        data: tours,
+    });
 });
 
 export const createBooking = createOne(Booking);
--- server/routes/bookingRoute.js
+++ server/routes/bookingRoute.js
@@ -3,14 +3,11 @@ import { protect, restrictTo } from '../controllers/authController.js';
 import {
     setBookingData,
     createBooking,
+    myTours,
 } from '../controllers/bookingController.js';
 const bookingRoute = express.Router();
 
-bookingRoute.get(
-    '/checkout/:tourId',
-    protect,
-    setBookingData,
-    createBooking
-);
+bookingRoute.get('/checkout/:tourId', protect, setBookingData, createBooking);
+bookingRoute.get('/my-tours', protect, myTours);
 
 export default bookingRoute;
